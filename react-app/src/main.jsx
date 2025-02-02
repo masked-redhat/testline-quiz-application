b9878436ac4e7e1be901f5668cb2ac3fac66be5d
Zed <skhull.coding@proton.me>
2025-02-02 23:44:18 +0530

    fix: remove unused functions

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
