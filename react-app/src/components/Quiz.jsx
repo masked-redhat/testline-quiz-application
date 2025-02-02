2d69a8ebc8911c61e167c1f7e6c36d760b395941
Zed <skhull.coding@proton.me>
2025-02-02 23:00:38 +0530

    feat: answer selection

--- react-app/src/components/Quiz.jsx
+++ react-app/src/components/Quiz.jsx
@@ -5,37 +5,58 @@ const Quiz = () => {
   const data = useQuizData();
   const [cqp, setcqp] = useState(0); // current question position
   const [questions, setQuestions] = useState([]); // questions
+  const [answered, setAnswered] = useState([]); // answered questions
 
   useEffect(() => {
     setQuestions(data?.questions);
   }, [data]); // when data changes
 
-  useEffect(() => {
-    setcqp(0); // set current to first question
-  }, [questions]); // when question sets in
-
+  // next question
   const forwardQuestion = () => {
     if (cqp === questions.length - 1) {
     } else setcqp(cqp + 1);
   };
 
+  // previous question
   const rewindQuestion = () => {
     if (cqp === 0) {
     } else setcqp(cqp - 1);
+  };
+
+  const markQuestionAnswered = (id, correct, answerId) => {
+    const _questions = [...questions];
+    for (const question of _questions) {
+      if (question.id === id) {
+        question.answered = true;
+        question.correct = correct;
+        question.answerId = answerId;
+      }
+    }
+    setQuestions(_questions);
   };
 
   if (data === null) return <p className="text-xl font-jost">Loading...</p>;
 
   return (
     <div className="flex flex-col gap-3 w-[800px]">
-      <Question data={questions?.[cqp] ?? {}} />
+      <Question
+        data={questions?.[cqp] ?? {}}
+        answered={answered}
+        setAnswered={setAnswered}
+        mark={markQuestionAnswered}
+      />
       <div className="flex justify-between gap-2">
-        <button onClick={rewindQuestion} disabled={cqp === 0} className="cursor-pointer px-6 py-2 text-lg font-sen bg-gray-100 rounded-md disabled:text-gray-400 select-none">
+        <button
+          onClick={rewindQuestion}
+          disabled={cqp === 0}
+          className="button-renext"
+        >
           &larr; Previous
         </button>
         <button
           onClick={forwardQuestion}
-          disabled={cqp === questions?.length - 1} className="cursor-pointer px-6 py-2 text-lg font-sen bg-gray-100 rounded-md disabled:text-gray-400 select-none"
+          disabled={cqp === questions?.length - 1}
+          className="button-renext"
         >
           Next &rarr;
         </button>
@@ -44,31 +65,48 @@ const Quiz = () => {
   );
 };
 
-const Question = ({ data }) => {
+const Question = ({ data, answered, setAnswered, mark }) => {
   const _data = useQuizData();
+  console.log(data);
   return (
-    <form className="flex gap-8 flex-col">
-      <header>
-        <h2 className="font-sen text-xl h-16">{data?.description}</h2>
+    <form
+      className="flex gap-3 flex-col"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <header className="flex gap-2 flex-col">
+        <h2 className="font-sen text-xl h-20 flex items-center py-1">
+          {data?.description}
+        </h2>
+        <div className="flex gap-5 font-sen">
+          <p>{data?.topic}</p>
+          {data?.answered ? <p className="text-green-500">Answered</p> : <p className="text-red-500">Unanswered</p>}
+        </div>
       </header>
       <section className="flex flex-col gap-3 font-jost font-semibold text-lg">
         {data?.options?.map((option) => (
           <button
-          key={option.id}
-          className="px-4 py-3 bg-gray-200 cursor-pointer"
-          onClick={(e) => handleAnswer(e, option.is_correct)}
-        >
-
-          {option.description}
-        </button>
+            key={option?.id}
+            className={`px-4 py-3 bg-gray-200 cursor-pointer rounded-md disabled:cursor-default border-2 ${
+              option?.id == data?.answerId
+                ? "border-green-500"
+                : "border-gray-200"
+            }`}
+            onClick={() => {
+              mark(data?.id, option?.is_correct, option?.id);
+            }}
+          >
+            {option.description}
+          </button>
         ))}
       </section>
     </form>
   );
 };
 
-const handleAnswer = (e, is_correct) => {
-  e.preventDefault();
+const handleAnswer = (option) => {
+  console.log(option);
 };
 
 export default Quiz;
