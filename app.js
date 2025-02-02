929f455fe45ea52ad25534314da78c36883e1fc6
Zed <skhull.coding@proton.me>
2025-02-02 23:51:17 +0530

    feat: serve react app

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3002;
+const port = 3000; // should be in env file
 
 app.use(cors()); // allowing cors policy
 
-app.get("/", async (_, res) => {
+app.use(express.static("./react-app/dist")); // public folder
+
+app.get("/data", async (_, res) => {
   const _res = await fetch("https://api.jsonserve.com/Uw5CrX");
 
   const data = await _res.json();
 
   res.status(200).json(data);
+});
+
+app.get("*", (_, res) => {
+  res.sendFile("index.html");
 });
 
 app.listen(port, () => {
